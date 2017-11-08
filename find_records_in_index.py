3dc8f0aa46a6965e73bfd5cc1e32680167a74d4e
Todd Trann <todd.trann@usask.ca>
2017-11-08 09:13:40 -0600

    Update script

--- find_records_in_index.py
+++ find_records_in_index.py
@@ -14,9 +14,14 @@ import re
 import os
 
 querylimit = 400
-cmd = "/opt/rdm/datasearch_client/datasearch-client"
+cmd = "/opt/rdm/search_client/search-client"
 host = "search.api.globus.org"
-index = "frdr-test"
+# Globus has changed from index NAMES to index UUIDs. Use one of the following for the index:
+# frdr:      "9be6dd95-48f0-48bb-82aa-c6577a988775"
+# frdr-test: "5f262052-d6cc-4a2d-8fee-79678088af04"
+# frdr-demo: "f317467b-98d4-43d1-b73a-fbeb6f0fd7d6"
+# frdr-dev:  "37169d3a-beff-4367-bc6e-78f7e39e902c"
+index = "f317467b-98d4-43d1-b73a-fbeb6f0fd7d6"
 queryobj = {"@datatype":"GSearchRequest","@version":"2016-11-09","advanced":True,"offset":0,"limit":querylimit,"q":"*","filters":[{"@datatype":"GFilter","@version":"2016-11-09","type":"match_any","field_name":"https://frdr\\.ca/schema/1\\.0#origin\\.id","values":[""]}]}
 
 if len(sys.argv) > 1:
