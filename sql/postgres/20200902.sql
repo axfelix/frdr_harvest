f4db16731cf21763780e76a8d58dec9c732201ba
Kelly Stathis <kelly.a.stathis@gmail.com>
2020-10-02 13:04:35 -0700

    Update geospatial tables with concise names and add bridge table for geooplace

--- sql/postgres/20200902.sql
+++ sql/postgres/20200902.sql
@@ -1,43 +1,51 @@
-CREATE TABLE geospatial_boundingbox (
-	geospatial_boundingbox_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE geobbox (
+	geobbox_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	westLon NUMERIC NOT NULL,
 	eastLon NUMERIC NOT NULL,
     northLat NUMERIC NOT NULL,
     southLat NUMERIC NOT NULL);
-CREATE INDEX geospatial_boundingbox_by_record on geospatial_boundingbox(record_id);
-CREATE SEQUENCE IF NOT EXISTS geospatial_boundingbox_id_sequence;
-ALTER TABLE geospatial_boundingbox ALTER geospatial_boundingbox_id SET DEFAULT NEXTVAL('geospatial_boundingbox_id_sequence');
+CREATE INDEX geobbox_by_record on geobbox(record_id);
+CREATE SEQUENCE IF NOT EXISTS geobbox_id_sequence;
+ALTER TABLE geobbox ALTER geobbox_id SET DEFAULT NEXTVAL('geobbox_id_sequence');
 
-
-CREATE TABLE geospatial_point (
-	geospatial_point_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE geopoint (
+	geopoint_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	lat NUMERIC NOT NULL,
 	lon NUMERIC NOT NULL);
-CREATE INDEX geospatial_point_by_record on geospatial_point(record_id);
-CREATE SEQUENCE IF NOT EXISTS geospatial_point_id_sequence;
-ALTER TABLE geospatial_point ALTER geospatial_point_id SET DEFAULT NEXTVAL('geospatial_point_id_sequence');
+CREATE INDEX geopoint_by_record on geopoint(record_id);
+CREATE SEQUENCE IF NOT EXISTS geopoint_id_sequence;
+ALTER TABLE geopoint ALTER geopoint_id SET DEFAULT NEXTVAL('geopoint_id_sequence');
 
-CREATE TABLE geospatial_location (
-	geospatial_location_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE geoplace (
+	geoplace_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	country TEXT,
-    province_state TEXT,
-    city TEXT,
-    other TEXT,
-    place_name TEXT);
-CREATE INDEX geospatial_location_by_record on  geospatial_location(record_id);
-CREATE SEQUENCE IF NOT EXISTS geospatial_location_id_sequence;
-ALTER TABLE geospatial_location ALTER geospatial_location_id SET DEFAULT NEXTVAL('geospatial_location_id_sequence');
+  province_state TEXT,
+  city TEXT,
+  other TEXT,
+  place_name TEXT);
+CREATE INDEX geoplace_by_record on  geoplace(record_id);
+CREATE SEQUENCE IF NOT EXISTS geoplace_id_sequence;
+ALTER TABLE geoplace ALTER geoplace_id SET DEFAULT NEXTVAL('geoplace_id_sequence');
 
-CREATE TABLE geospatial_file (
-	geospatial_file_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE records_x_geoplace (
+	records_x_geoplace_id INTEGER PRIMARY KEY NOT NULL,
+	record_id INTEGER NOT NULL,
+	geoplace_id INTEGER NOT NULL);
+
+CREATE SEQUENCE IF NOT EXISTS records_x_geoplace_seq;
+ALTER TABLE records_x_geoplace ALTER records_x_geoplace_id SET DEFAULT NEXTVAL('records_x_geoplace_seq');
+
+CREATE TABLE geofile (
+	geofile_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	server_name TEXT NOT NULL,
 	format TEXT,
 	filename TEXT NOT NULL,
     uri TEXT NOT NULL);
-CREATE INDEX  geospatial_file_by_record on   geospatial_file(record_id);
-CREATE SEQUENCE IF NOT EXISTS  geospatial_file_id_sequence;
-ALTER TABLE geospatial_file ALTER  geospatial_file_id SET DEFAULT NEXTVAL('geospatial_file_id_sequence');
+
+CREATE INDEX  geofile_by_record on   geofile(record_id);
+CREATE SEQUENCE IF NOT EXISTS  geofile_id_sequence;
+ALTER TABLE geofile ALTER  geofile_id SET DEFAULT NEXTVAL('geofile_id_sequence');
--- sql/sqlite/20200902.sql
+++ sql/sqlite/20200902.sql
@@ -1,35 +1,39 @@
-CREATE TABLE geospatial_boundingbox (
-	geospatial_boundingbox_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE geobbox(
+	geobbox_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	westLon NUMERIC NOT NULL,
 	eastLon NUMERIC NOT NULL,
     northLat NUMERIC NOT NULL,
     southLat NUMERIC NOT NULL);
-CREATE INDEX geospatial_boundingbox_by_record on geospatial_boundingbox(record_id);
+CREATE INDEX geobbox_by_record on geobbox(record_id);
 
-CREATE TABLE geospatial_point (
-	geospatial_point_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE geopoint (
+	geopoint_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	lat NUMERIC NOT NULL,
 	lon NUMERIC NOT NULL);
-CREATE INDEX geospatial_point_by_record on geospatial_point(record_id);
+CREATE INDEX geopoint_by_record on geopoint(record_id);
 
-CREATE TABLE geospatial_location (
-	geospatial_location_id INTEGER PRIMARY KEY NOT NULL,
-	record_id INTEGER NOT NULL,
+CREATE TABLE geoplace (
+	geoplace_id INTEGER PRIMARY KEY NOT NULL,
 	country TEXT,
     province_state TEXT,
     city TEXT,
     other TEXT,
     place_name TEXT);
-CREATE INDEX geospatial_location_by_record on  geospatial_location(record_id);
 
-CREATE TABLE geospatial_file (
-	geospatial_file_id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE records_x_geoplace (
+	records_x_geoplace_id INTEGER PRIMARY KEY NOT NULL,
+	record_id INTEGER NOT NULL,
+	geoplace_id INTEGER NOT NULL);
+CREATE INDEX records_x_geoplace_by_record on records_x_geoplace(record_id);
+CREATE INDEX records_x_geoplace_by_geoplace on records_x_geoplace(geoplace_id);
+
+CREATE TABLE geofile (
+	geofile_id INTEGER PRIMARY KEY NOT NULL,
 	record_id INTEGER NOT NULL,
 	server_name TEXT NOT NULL,
 	format TEXT,
 	filename TEXT NOT NULL,
     uri TEXT NOT NULL);
-CREATE INDEX  geospatial_file_by_record on   geospatial_file(record_id);
-
+CREATE INDEX  geofileby_record on   geofile(record_id);
