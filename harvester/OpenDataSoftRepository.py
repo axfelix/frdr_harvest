f7c1ce9a796c1e26e67dbea2650682b808d93dab
Kelly Stathis <kelly.a.stathis@gmail.com>
2021-02-22 16:35:47 -0800

    DataCite API harvesting (#12)
    * Add support for DataCite API
    
    * Check for Dataset type
    
    * Ensure replacement date is more complete than publication year
    
    * Add affiliations
    
    * Refactoring and comments
    
    * Delete records that are not "Dataset" type
    
    * Remove "affiliation" from record if empty; fix geobboxes
    
    * Check that creator has "name" before accessing
    
    * Specify page_size of 1000 and write headers as each page is retrieved
    
    * Fix OpenDataSoft logger
    
    * Add cursor support for repositories with > 10K DOIs
    
    * Fix cursor implementation
    
    * Remove conditional for BOLD/CBG

--- harvester/OpenDataSoftRepository.py
+++ harvester/OpenDataSoftRepository.py
@@ -58,8 +58,8 @@ class OpenDataSoftRepository(HarvestRepository):
                     item_count = item_count + 1
                     if (item_count % self.update_log_after_numitems == 0):
                         tdelta = time.time() - self.tstart + 0.1
-                        self.logger.info("Done {} item headers after {} ({:.1f} items/sec)".format(item_count,
-                                                                            item_count / tdelta))
+                        self.logger.info("Done {} item headers after {} ({:.1f} items/sec)".format(item_count,self.formatter.humanize(
+                                                                                               tdelta),item_count / tdelta))
                 offset += self.records_per_request
             self.logger.info("Found {} items in feed".format(item_count))
 
