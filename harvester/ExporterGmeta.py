561e753a3916e1af954be818f4a38745712158df
Todd Trann <toddtrann@sasktel.net>
2021-03-30 23:37:57 -0600

    That time Todd remembered lists are not strings

--- harvester/ExporterGmeta.py
+++ harvester/ExporterGmeta.py
@@ -206,10 +206,14 @@ class ExporterGmeta(Exporter.Exporter):
             record["frdr_origin_icon"] = record["repository_thumbnail"]
 
             # Concatenate EN and FR into multi-language fields for Globus search
-            record["dc_title_multi"]       = record["dc_title_en"]       + " " + record["dc_title_fr"]
-            record["frdr_subject_multi"]   = record["frdr_subject_en"]   + " " + record["frdr_subject_fr"]
-            record["frdr_keyword_multi"]   = record["frdr_keyword_en"]   + " " + record["frdr_keyword_fr"]
+            record["dc_title_multi"]       = record["dc_title_en"] + " " + record["dc_title_fr"]
             record["dc_description_multi"] = record["dc_description_en"] + " " + record["dc_description_fr"]
+            record["frdr_subject_multi"] = []
+            record["frdr_subject_multi"].extend(record["frdr_subject_en"])
+            record["frdr_subject_multi"].extend(record["frdr_subject_fr"])
+            record["frdr_keyword_multi"] = []
+            record["frdr_keyword_multi"].extend(record["frdr_keyword_en"])
+            record["frdr_keyword_multi"].extend(record["frdr_keyword_fr"])
 
             # remove unneeded columns from output
             record.pop("contact", None)
